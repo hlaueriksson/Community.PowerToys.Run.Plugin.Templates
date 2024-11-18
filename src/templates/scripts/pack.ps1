c288883552db153a7c06fca5885f4453fdb96f13
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2024-11-18 20:34:35 +0100

    scripts

---
+++ src/templates/scripts/pack.ps1
@@ -0,0 +1,76 @@
+<#PSScriptInfo
+.VERSION 0.86.0
+.GUID 58d7b8e8-fa18-485d-baaf-4c413181280b
+.AUTHOR Henrik Lau Eriksson
+.COMPANYNAME
+.COPYRIGHT
+.TAGS PowerToys Run Plugins Pack
+.LICENSEURI
+.PROJECTURI https://github.com/hlaueriksson/Community.PowerToys.Run.Plugin.Templates
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES
+#>
+
+<#
+    .Synopsis
+    Packs the plugins into release archives.
+
+    .Description
+    Builds the solution in Release configuration,
+    copies the output files into plugin folders,
+    packs the plugin folders into release archives.
+
+    .Example
+    .\pack.ps1
+
+    .Link
+    https://github.com/hlaueriksson/Community.PowerToys.Run.Plugin.Templates
+#>
+
+# Clean
+Get-ChildItem -Path "." -Directory -Include "bin", "obj" -Recurse | Remove-Item -Recurse -Force
+
+$dependencies = @("PowerToys.Common.UI.*", "PowerToys.ManagedCommon.*", "PowerToys.Settings.UI.Lib.*", "Wox.Infrastructure.*", "Wox.Plugin.*")
+
+# Plugins
+$folders = Get-ChildItem -Recurse -Filter "plugin.json" | Where-Object { $_.FullName -notlike "*\bin\*" } | ForEach-Object { $_.Directory } | Sort-Object -Unique
+
+Write-Output "Pack:"
+foreach ($folder in $folders) {
+    Write-Output "- $($folder.Name)"
+
+    $name = $($folder.Name.Split(".")[-1])
+
+    # Version
+    $json = Get-Content -Path (Join-Path $folder.FullName "plugin.json") -Raw | ConvertFrom-Json
+    $version = $json.Version
+    Write-Output "Version: $version"
+
+    # Platforms
+    [xml]$csproj = Get-Content -Path (Join-Path $folder.FullName "*.csproj")
+    $targetFramework = $csproj.Project.PropertyGroup.TargetFramework
+    $platforms = "$($csproj.Project.PropertyGroup.Platforms)".Trim() -split ";"
+
+    foreach ($platform in $platforms)
+    {
+        Write-Output "Platform: $platform"
+
+        # Build
+        dotnet build $folder -c Release /p:TF_BUILD=true /p:Platform=$platform
+
+        if (!$?) {
+            # Build FAILED.
+            Exit $LastExitCode
+        }
+
+        $output = "$folder\bin\$platform\Release\$targetFramework\"
+        $destination = "$folder\bin\$platform\$name"
+        $zip = "$folder\bin\$platform\$name-$version-$($platform.ToLower()).zip"
+
+        Copy-Item -Path $output -Destination $destination -Recurse -Exclude $dependencies
+        Compress-Archive -Path $destination -DestinationPath $zip
+    }
+}
---
+++ src/templates/scripts/releasenotes.ps1
@@ -0,0 +1,101 @@
+<#PSScriptInfo
+.VERSION 0.86.0
+.GUID d790e6d3-96c9-447b-9863-941da73870ea
+.AUTHOR Henrik Lau Eriksson
+.COMPANYNAME
+.COPYRIGHT
+.TAGS PowerToys Run Plugins Release
+.LICENSEURI
+.PROJECTURI https://github.com/hlaueriksson/Community.PowerToys.Run.Plugin.Templates
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES
+#>
+
+<#
+    .Synopsis
+    Generate release notes snippets for the plugins.
+
+    .Description
+    Gathers information about the plugins and generates a markdown file with release notes snippets.
+
+    .Example
+    .\releasenotes.ps1
+
+    .Link
+    https://github.com/hlaueriksson/Community.PowerToys.Run.Plugin.Templates
+#>
+
+function Write-Line {
+    param (
+        [string]$line
+    )
+
+    $line | Add-Content -Path $result
+}
+
+function Get-Platform {
+    param (
+        [string]$filename
+    )
+
+    if ($filename -Match $platforms[0]) {
+        $platforms[0]
+    }
+    else {
+        $platforms[1]
+    }
+}
+
+# Output
+$result = "releasenotes.md"
+
+Set-Content -Path $result -Value ""
+
+# Plugins
+$folders = Get-ChildItem -Recurse -Filter "plugin.json" | Where-Object { $_.FullName -notlike "*\bin\*" } | ForEach-Object { $_.Directory } | Sort-Object -Unique
+
+Write-Output "Doc:"
+foreach ($folder in $folders) {
+    Write-Output "- $($folder.Name)"
+
+    $name = $($folder.Name.Split(".")[-1])
+
+    # Version, Website
+    $json = Get-Content -Path (Join-Path $folder.FullName "plugin.json") -Raw | ConvertFrom-Json
+    $version = $json.Version
+    $website = $json.Website
+
+    # Platforms
+    [xml]$csproj = Get-Content -Path (Join-Path $folder.FullName "*.csproj")
+    $platforms = "$($csproj.Project.PropertyGroup.Platforms)".Trim() -split ";"
+
+    $files = Get-ChildItem -Path $folder -File -Include "$name-$version*.zip" -Recurse
+
+    Write-Line "## $name"
+    Write-Line ""
+    Write-Line "| Platform | Filename | Downloads"
+    Write-Line "| --- | --- | ---"
+    foreach ($file in $files) {
+        $zip = $file.Name
+        $platform = Get-Platform $zip
+        $url = "$website/releases/download/v$version/$zip"
+        $badge = "https://img.shields.io/github/downloads/$($website.Replace('https://github.com/', ''))/v$version/$zip"
+
+        Write-Line "| ``$platform`` | [$zip]($url) | [![$zip]($badge)]($url)"
+    }
+    Write-Line ""
+
+    Write-Line "### Installer Hashes"
+    Write-Line ""
+    Write-Line "| Filename | SHA256 Hash"
+    Write-Line "| --- | ---"
+    foreach ($file in $files) {
+        $zip = $file.Name
+        $hash = Get-FileHash $file -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+
+        Write-Line "| ``$zip`` | ``$hash``"
+    }
+}
