0d6bcb4c1d7661eed6e526c2e5897d157238c56c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2025-12-30 15:28:38 +0100

    File Scoped Namespace

--- src/templates/project/Main.cs
+++ src/templates/project/Main.cs
@@ -5,132 +5,131 @@ using System.Windows;
 using System.Windows.Input;
 using Wox.Plugin;
 
-namespace Namespace.Plugin1
+namespace Namespace.Plugin1;
+
+/// <summary>
+/// Main class of this plugin that implement all used interfaces.
+/// </summary>
+public class Main : IPlugin, IContextMenu, IDisposable
 {
     /// <summary>
-    /// Main class of this plugin that implement all used interfaces.
+    /// ID of the plugin.
     /// </summary>
-    public class Main : IPlugin, IContextMenu, IDisposable
+    public static string PluginID => "00000000000000000000000000000000";
+
+    /// <summary>
+    /// Name of the plugin.
+    /// </summary>
+    public string Name => "Plugin1";
+
+    /// <summary>
+    /// Description of the plugin.
+    /// </summary>
+    public string Description => "Plugin1 Description";
+
+    private PluginInitContext Context { get; set; }
+
+    private string IconPath { get; set; }
+
+    private bool Disposed { get; set; }
+
+    /// <summary>
+    /// Return a filtered list, based on the given query.
+    /// </summary>
+    /// <param name="query">The query to filter the list.</param>
+    /// <returns>A filtered list, can be empty when nothing was found.</returns>
+    public List<Result> Query(Query query)
     {
-        /// <summary>
-        /// ID of the plugin.
-        /// </summary>
-        public static string PluginID => "00000000000000000000000000000000";
+        var search = query.Search;
 
-        /// <summary>
-        /// Name of the plugin.
-        /// </summary>
-        public string Name => "Plugin1";
+        return
+        [
+            new Result
+            {
+                QueryTextDisplay = search,
+                IcoPath = IconPath,
+                Title = "Title: " + search,
+                SubTitle = "SubTitle",
+                ToolTipData = new ToolTipData("Title", "Text"),
+                Action = _ =>
+                {
+                    Clipboard.SetDataObject(search);
+                    return true;
+                },
+                ContextData = search,
+            }
+        ];
+    }
 
-        /// <summary>
-        /// Description of the plugin.
-        /// </summary>
-        public string Description => "Plugin1 Description";
+    /// <summary>
+    /// Initialize the plugin with the given <see cref="PluginInitContext"/>.
+    /// </summary>
+    /// <param name="context">The <see cref="PluginInitContext"/> for this plugin.</param>
+    public void Init(PluginInitContext context)
+    {
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+        Context.API.ThemeChanged += OnThemeChanged;
+        UpdateIconPath(Context.API.GetCurrentTheme());
+    }
 
-        private PluginInitContext Context { get; set; }
-
-        private string IconPath { get; set; }
-
-        private bool Disposed { get; set; }
-
-        /// <summary>
-        /// Return a filtered list, based on the given query.
-        /// </summary>
-        /// <param name="query">The query to filter the list.</param>
-        /// <returns>A filtered list, can be empty when nothing was found.</returns>
-        public List<Result> Query(Query query)
+    /// <summary>
+    /// Return a list context menu entries for a given <see cref="Result"/> (shown at the right side of the result).
+    /// </summary>
+    /// <param name="selectedResult">The <see cref="Result"/> for the list with context menu entries.</param>
+    /// <returns>A list context menu entries.</returns>
+    public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+    {
+        if (selectedResult.ContextData is string search)
         {
-            var search = query.Search;
-
             return
             [
-                new Result
+                new ContextMenuResult
                 {
-                    QueryTextDisplay = search,
-                    IcoPath = IconPath,
-                    Title = "Title: " + search,
-                    SubTitle = "SubTitle",
-                    ToolTipData = new ToolTipData("Title", "Text"),
+                    PluginName = Name,
+                    Title = "Copy to clipboard (Ctrl+C)",
+                    FontFamily = "Segoe MDL2 Assets",
+                    Glyph = "\xE8C8", // Copy
+                    AcceleratorKey = Key.C,
+                    AcceleratorModifiers = ModifierKeys.Control,
                     Action = _ =>
                     {
                         Clipboard.SetDataObject(search);
                         return true;
                     },
-                    ContextData = search,
                 }
             ];
         }
 
-        /// <summary>
-        /// Initialize the plugin with the given <see cref="PluginInitContext"/>.
-        /// </summary>
-        /// <param name="context">The <see cref="PluginInitContext"/> for this plugin.</param>
-        public void Init(PluginInitContext context)
-        {
-            Context = context ?? throw new ArgumentNullException(nameof(context));
-            Context.API.ThemeChanged += OnThemeChanged;
-            UpdateIconPath(Context.API.GetCurrentTheme());
-        }
-
-        /// <summary>
-        /// Return a list context menu entries for a given <see cref="Result"/> (shown at the right side of the result).
-        /// </summary>
-        /// <param name="selectedResult">The <see cref="Result"/> for the list with context menu entries.</param>
-        /// <returns>A list context menu entries.</returns>
-        public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
-        {
-            if (selectedResult.ContextData is string search)
-            {
-                return
-                [
-                    new ContextMenuResult
-                    {
-                        PluginName = Name,
-                        Title = "Copy to clipboard (Ctrl+C)",
-                        FontFamily = "Segoe MDL2 Assets",
-                        Glyph = "\xE8C8", // Copy
-                        AcceleratorKey = Key.C,
-                        AcceleratorModifiers = ModifierKeys.Control,
-                        Action = _ =>
-                        {
-                            Clipboard.SetDataObject(search);
-                            return true;
-                        },
-                    }
-                ];
-            }
-
-            return [];
-        }
-
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Wrapper method for <see cref="Dispose()"/> that dispose additional objects and events form the plugin itself.
-        /// </summary>
-        /// <param name="disposing">Indicate that the plugin is disposed.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (Disposed || !disposing)
-            {
-                return;
-            }
-
-            if (Context?.API != null)
-            {
-                Context.API.ThemeChanged -= OnThemeChanged;
-            }
-
-            Disposed = true;
-        }
-
-        private void UpdateIconPath(Theme theme) => IconPath = theme == Theme.Light || theme == Theme.HighContrastWhite ? "Images/plugin1.light.png" : "Images/plugin1.dark.png";
-
-        private void OnThemeChanged(Theme currentTheme, Theme newTheme) => UpdateIconPath(newTheme);
+        return [];
     }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Wrapper method for <see cref="Dispose()"/> that dispose additional objects and events form the plugin itself.
+    /// </summary>
+    /// <param name="disposing">Indicate that the plugin is disposed.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (Disposed || !disposing)
+        {
+            return;
+        }
+
+        if (Context?.API != null)
+        {
+            Context.API.ThemeChanged -= OnThemeChanged;
+        }
+
+        Disposed = true;
+    }
+
+    private void UpdateIconPath(Theme theme) => IconPath = theme == Theme.Light || theme == Theme.HighContrastWhite ? "Images/plugin1.light.png" : "Images/plugin1.dark.png";
+
+    private void OnThemeChanged(Theme currentTheme, Theme newTheme) => UpdateIconPath(newTheme);
 }
--- src/templates/solution/Community.PowerToys.Run.Plugin.Plugin1.UnitTests/MainTests.cs
+++ src/templates/solution/Community.PowerToys.Run.Plugin.Plugin1.UnitTests/MainTests.cs
@@ -2,33 +2,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using Wox.Plugin;
 
-namespace Community.PowerToys.Run.Plugin.Plugin1.UnitTests
+namespace Community.PowerToys.Run.Plugin.Plugin1.UnitTests;
+
+[TestClass]
+public class MainTests
 {
-    [TestClass]
-    public class MainTests
+    private Main main;
+
+    [TestInitialize]
+    public void TestInitialize()
     {
-        private Main main;
+        main = new Main();
+    }
 
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            main = new Main();
-        }
+    [TestMethod]
+    public void Query_should_return_results()
+    {
+        var results = main.Query(new("search"));
 
-        [TestMethod]
-        public void Query_should_return_results()
-        {
-            var results = main.Query(new("search"));
+        Assert.IsNotNull(results.First());
+    }
 
-            Assert.IsNotNull(results.First());
-        }
+    [TestMethod]
+    public void LoadContextMenus_should_return_results()
+    {
+        var results = main.LoadContextMenus(new Result { ContextData = "search" });
 
-        [TestMethod]
-        public void LoadContextMenus_should_return_results()
-        {
-            var results = main.LoadContextMenus(new Result { ContextData = "search" });
-
-            Assert.IsNotNull(results.First());
-        }
+        Assert.IsNotNull(results.First());
     }
 }
